694d081da745fc10fc165a7dbf6c146eb2b81d1b
codEinstein-prog <131826510+codEinstein-prog@users.noreply.github.com>
2025-06-03 22:09:05 -0500

    Update signup.inc.php

--- signup.inc.php
+++ signup.inc.php
@@ -7,10 +7,38 @@
     $userName = $_POST["Uid"];
     $pwd = $_POST["pwd"];
     $pwdRepeat = $_POST["pwdRepeat"];
-  }
+  
 
-require_once 'database.inc.php';
-require_once 'functions.inc.php';
+    require_once 'database.inc.php';
+    require_once 'functions.inc.php';
+  
+    if(emptyInputSignup($name, $email, $userName, $pwd, $pwdRepeat) !== false){
+       header("location: ../signup.php?error=emptyinput");
+       exit();
+    }
+  
+    if(InvalidUid($userName) !== false){
+       header("location: ../signup.php?error=InvalidUid");
+      exit();
+    }
+    
+    if(Invalidemail($email) !== false){
+       header("location: ../signup.php?error=Invalidemail");
+      exit();
+    }
+    
+    if(pwdMatch($pwd, $pwdRepeat) !== false){
+       header("location: ../signup.php?error=passwordsdontmatch");
+      exit();
+    }
+    
+    if(UidExist($conn, $userName) !== false){
+       header("location: ../signup.php?error=usernamestaken");
+      exit();
+    }
+    
+    createUser($conn, $name, $email, $username, pwd);
+}
 
 else{
   header("location: ../signup.php");
